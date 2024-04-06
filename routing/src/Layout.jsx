e3a8da8faef5c79ce55c652093ae61230ed76724
Sanjaya Niroula <niroulasanjaya42@fmail.com>
2024-04-06 23:31:21 +0545

    progress

--- routing/src/Layout.jsx
+++ routing/src/Layout.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import User from './components/User/User'
 
 function layout() {
   return (
@@ -9,6 +12,9 @@ function layout() {
 
    <Header />
    <Outlet />
+   <About/>
+   <Contact/>
+   <User/>
    <Footer />
 
   </>
--- routing/src/main.jsx
+++ routing/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
-
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -12,7 +15,7 @@ const router = createBrowserRouter(
 
       <Route path='' element= {<Home />} />
       <Route path='about' element= {<About />} />
-      
+      <Route path='user' element= { <User/> } />
     </Route>
   )
 )
