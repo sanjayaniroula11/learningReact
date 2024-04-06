3442bc32d0aa86c122a8611705943414eab59d3d
Sanjaya Niroula <niroulasanjaya42@fmail.com>
2024-04-06 23:34:27 +0545

    PROP

--- routing/src/main.jsx
+++ routing/src/main.jsx
@@ -15,6 +15,7 @@ const router = createBrowserRouter(
 
       <Route path='' element= {<Home />} />
       <Route path='about' element= {<About />} />
+      <Route path='contact' element= {<Contact />} />
       <Route path='user' element= { <User/> } />
     </Route>
   )
