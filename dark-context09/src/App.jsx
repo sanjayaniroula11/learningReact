72e04109bf5fc8ccff52bdb717de967ba3ac3c2d
Sanjaya Niroula <niroulasanjaya42@fmail.com>
2024-05-09 14:32:09 +0545

    darkmode-2

--- dark-context09/src/App.jsx
+++ dark-context09/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
 
-  const [ themeMode, setThemeMode] = useState('light')
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
 
 
-  const lightTheme = ()=>{
-    setThemeMode('light')
-  }
-  const darkTheme = ()=>{
-    setThemeMode('dark')
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
 
-  }
+
+  //How changing of theme happens
+
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light','dark')
+    document.querySelector('html').classList.add('themeMode')
+  }, [themeMode])
+  
+  
+
 
   return (
-  <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+
+
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
 
 
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-          {/* themebtn */}
-          <div className="w-full max-w-sm mx-auto">{/* card */}</div>
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"><ThemeBtn/></div>
+          
+          <div className="w-full max-w-sm mx-auto"><Card /></div>
         </div>
       </div>
 
 
     </ThemeProvider>
+
+
   );
 }
 
--- dark-context09/src/components/Card.jsx
+++ dark-context09/src/components/Card.jsx
@@ -4,12 +4,12 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/1996333/pexels-photo-1996333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Stallian Horse
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -63,7 +63,7 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$0.5 Million</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
